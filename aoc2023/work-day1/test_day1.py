f44d7fa69d187cd749764542c3deaa695b6845b6
Johannes Knopp <jkn@solute.de>
2024-11-18 14:36:46 +0100

    aoc23, d1, p2: Add test

--- aoc2023/work-day1/test_day1.py
+++ aoc2023/work-day1/test_day1.py
@@ -13,3 +13,17 @@ def test_part1() -> None:
         results.append(day1.digits_to_number(d1, d2))
     assert calibration_values == results
     assert sum(calibration_values) == total == day1.compute_result(test_data)
+
+
+def test_part2() -> None:
+    with open("data/test_part2.txt") as f:
+        test_data = list(day1.read_input(f))
+    calibration_values = [29, 83, 13, 24, 42, 14, 76]
+    total = 281
+
+    results = []
+    for line in test_data:
+        d1, d2 = day1.find_first_and_last_digit(line)
+        results.append(day1.digits_to_number(d1, d2))
+    assert calibration_values == results
+    assert sum(calibration_values) == total == day1.compute_result(test_data)
